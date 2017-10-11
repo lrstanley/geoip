ed1699db4bb4a720f58871b595191287f89b7c74
Liam Stanley <me@liamstanley.io>
2017-10-11 03:20:04 -0400

    frontend: spruce up docs page layout, TODO

--- public/src/components/Docs.vue
+++ public/src/components/Docs.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <h1>Docs content here</h1>
+    <h2 class="ui header">
+      <i class="settings icon"></i>
+      <div class="content">
+        API Documentation
+        <div class="sub header">Query the GeoIP API through <strong>REST</strong> and <strong>JSON</strong></div>
+      </div>
+    </h2>
+
+    <div class="ui divider"></div>
+    <p style="margin: 100px 0;">TODO</p>
   </div>
 </template>
 
