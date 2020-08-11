aebebf6f2ec162bc133721887c828c5d677a51b1
Liam Stanley <me@liamstanley.io>
2020-08-10 23:55:23 -0400

    add env support for all flags

--- README.md
+++ README.md
@@ -91,35 +91,35 @@ Usage:
   geoip [OPTIONS]
 
 Application Options:
-  -d, --debug          enable exception display and pprof endpoints (warn: dangerous)
-  -q, --quiet          disable verbose output
-      --db=            path to read/store Maxmind DB (default: geoip.db)
-      --interval=      interval of time between database update checks (default: 12h)
-      --update-url=    maxmind database file download location (must be gzipped) (default:
-                       https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=%s&suffix=tar.gz) [$MAXMIND_UPDATE_URL]
+  -d, --debug          enable exception display and pprof endpoints (warn: dangerous) [$DEBUG]
+  -q, --quiet          disable verbose output [$QUIET]
+      --db=            path to read/store Maxmind DB (default: geoip.db) [$DB_PATH]
+      --interval=      interval of time between database update checks (default: 12h) [$UPDATE_INTERVAL]
+      --update-url=    maxmind database file download location (must be gzipped) (default: https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=%s&suffix=tar.gz)
+                       [$MAXMIND_UPDATE_URL]
       --license-key=   maxmind license key (must register for a maxmind account) [$MAXMIND_LICENSE_KEY]
   -v, --version        print the version and compilation date
 
 Cache Options:
-      --cache.size=    total number of lookups to keep in ARC cache (50% most recent, 50% most requested) (default: 500)
-      --cache.expire=  expiration time of cache (default: 20m)
+      --cache.size=    total number of lookups to keep in ARC cache (50% most recent, 50% most requested) (default: 500) [$CACHE_SIZE]
+      --cache.expire=  expiration time of cache (default: 20m) [$CACHE_EXPIRE]
 
 HTTP Options:
-  -b, --http.bind=     address and port to bind to (default: :8080)
-      --http.proxy     obey X-Forwarded-For headers (warn: dangerous, make sure to only bind to localhost)
-      --http.throttle= limit total max concurrent requests across all connections
-      --http.limit=    number of requests/ip/hour (default: 2000)
-      --http.cors=     cors origin domain to allow with https?:// prefix (empty => '*'; use flag multiple times)
+  -b, --http.bind=     address and port to bind to (default: :8080) [$HTTP_BIND]
+      --http.proxy     obey X-Forwarded-For headers (warn: dangerous, make sure to only bind to localhost) [$HTTP_PROXY]
+      --http.throttle= limit total max concurrent requests across all connections [$HTTP_THROTTLE]
+      --http.limit=    number of requests/ip/hour (default: 2000) [$HTTP_LIMIT]
+      --http.cors=     cors origin domain to allow with https?:// prefix (empty => '*'; use flag multiple times) [$HTTP_CORS]
 
 TLS Options:
-      --http.tls.use   enable tls
-      --http.tls.cert= path to ssl certificate
-      --http.tls.key=  path to ssl key
+      --http.tls.use   enable tls [$TLS_USE]
+      --http.tls.cert= path to ssl certificate [$TLS_CERT]
+      --http.tls.key=  path to ssl key [$TLS_KEY]
 
 DNS Lookup Options:
-      --dns.timeout=   max allowed duration when looking up hostnames (may cause queries to be slow) (default: 2s)
-      --dns.resolver=  resolver (in host:port form) to use for dns lookups (doesn't work with windows and plan9) (can be used multiple times)
-      --dns.uselocal   adds local (system) resolvers to the list of resolvers to use
+      --dns.timeout=   max allowed duration when looking up hostnames (may cause queries to be slow) (default: 2s) [$DNS_TIMEOUT]
+      --dns.resolver=  resolver (in host:port form) to use for dns lookups (doesn't work with windows and plan9) (can be used multiple times) [$DNS_RESOLVERS]
+      --dns.uselocal   adds local (system) resolvers to the list of resolvers to use [$DNS_LOCAL]
 
 Help Options:
   -h, --help           Show this help message
--- main.go
+++ main.go
@@ -29,32 +29,32 @@ var (
 )
 
 type Flags struct {
-	Debug          bool          `short:"d" long:"debug" description:"enable exception display and pprof endpoints (warn: dangerous)"`
-	Quiet          bool          `short:"q" long:"quiet" description:"disable verbose output"`
-	DBPath         string        `long:"db" description:"path to read/store Maxmind DB" default:"geoip.db"`
-	UpdateInterval time.Duration `long:"interval" description:"interval of time between database update checks" default:"12h"`
-	UpdateURL      string        `long:"update-url" env:"MAXMIND_UPDATE_URL" description:"maxmind database file download location (must be gzipped)" default:"https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=%s&suffix=tar.gz"`
-	LicenseKey     string        `long:"license-key" env:"MAXMIND_LICENSE_KEY" description:"maxmind license key (must register for a maxmind account)" required:"true"`
+	Debug          bool          `env:"DEBUG" short:"d" long:"debug" description:"enable exception display and pprof endpoints (warn: dangerous)"`
+	Quiet          bool          `env:"QUIET" short:"q" long:"quiet" description:"disable verbose output"`
+	DBPath         string        `env:"DB_PATH" long:"db" description:"path to read/store Maxmind DB" default:"geoip.db"`
+	UpdateInterval time.Duration `env:"UPDATE_INTERVAL" long:"interval" description:"interval of time between database update checks" default:"12h"`
+	UpdateURL      string        `env:"MAXMIND_UPDATE_URL" long:"update-url" description:"maxmind database file download location (must be gzipped)" default:"https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=%s&suffix=tar.gz"`
+	LicenseKey     string        `env:"MAXMIND_LICENSE_KEY" long:"license-key" description:"maxmind license key (must register for a maxmind account)" required:"true"`
 	Cache          struct {
-		Size   int           `long:"size" description:"total number of lookups to keep in ARC cache (50% most recent, 50% most requested)" default:"500"`
-		Expire time.Duration `long:"expire" description:"expiration time of cache" default:"20m"`
+		Size   int           `env:"CACHE_SIZE" long:"size" description:"total number of lookups to keep in ARC cache (50% most recent, 50% most requested)" default:"500"`
+		Expire time.Duration `env:"CACHE_EXPIRE" long:"expire" description:"expiration time of cache" default:"20m"`
 	} `group:"Cache Options" namespace:"cache"`
 	HTTP struct {
-		Bind     string   `short:"b" long:"bind" description:"address and port to bind to" default:":8080"`
-		Proxy    bool     `long:"proxy" description:"obey X-Forwarded-For headers (warn: dangerous, make sure to only bind to localhost)"`
-		Throttle int      `long:"throttle" description:"limit total max concurrent requests across all connections"`
-		Limit    int      `long:"limit" description:"number of requests/ip/hour" default:"2000"`
-		CORS     []string `long:"cors" description:"cors origin domain to allow with https?:// prefix (empty => '*'; use flag multiple times)"`
+		Bind     string   `env:"HTTP_BIND" short:"b" long:"bind" description:"address and port to bind to" default:":8080"`
+		Proxy    bool     `env:"HTTP_PROXY" long:"proxy" description:"obey X-Forwarded-For headers (warn: dangerous, make sure to only bind to localhost)"`
+		Throttle int      `env:"HTTP_THROTTLE" long:"throttle" description:"limit total max concurrent requests across all connections"`
+		Limit    int      `env:"HTTP_LIMIT" long:"limit" description:"number of requests/ip/hour" default:"2000"`
+		CORS     []string `env:"HTTP_CORS" long:"cors" description:"cors origin domain to allow with https?:// prefix (empty => '*'; use flag multiple times)"`
 		TLS      struct {
-			Use  bool   `long:"use" description:"enable tls"`
-			Cert string `long:"cert" description:"path to ssl certificate"`
-			Key  string `long:"key" description:"path to ssl key"`
+			Use  bool   `env:"TLS_USE" long:"use" description:"enable tls"`
+			Cert string `env:"TLS_CERT" long:"cert" description:"path to ssl certificate"`
+			Key  string `env:"TLS_KEY" long:"key" description:"path to ssl key"`
 		} `group:"TLS Options" namespace:"tls"`
 	} `group:"HTTP Options" namespace:"http"`
 	DNS struct {
-		Timeout   time.Duration `long:"timeout" description:"max allowed duration when looking up hostnames (may cause queries to be slow)" default:"2s"`
-		Resolvers []string      `long:"resolver" description:"resolver (in host:port form) to use for dns lookups (doesn't work with windows and plan9) (can be used multiple times)"`
-		Local     bool          `long:"uselocal" description:"adds local (system) resolvers to the list of resolvers to use"`
+		Timeout   time.Duration `env:"DNS_TIMEOUT" long:"timeout" description:"max allowed duration when looking up hostnames (may cause queries to be slow)" default:"2s"`
+		Resolvers []string      `env:"DNS_RESOLVERS" long:"resolver" description:"resolver (in host:port form) to use for dns lookups (doesn't work with windows and plan9) (can be used multiple times)"`
+		Local     bool          `env:"DNS_LOCAL" long:"uselocal" description:"adds local (system) resolvers to the list of resolvers to use"`
 	} `group:"DNS Lookup Options" namespace:"dns"`
 	Version bool `short:"v" long:"version" description:"print the version and compilation date"`
 }
