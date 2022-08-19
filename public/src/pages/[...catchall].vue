6b05a0685b643ab9aed4ca44f4b5b7e1bb28cfde
Liam Stanley <me@liamstanley.io>
2022-08-19 01:03:25 -0400

    refactor catch-all
    Signed-off-by: Liam Stanley <me@liamstanley.io>

--- public/src/pages/[...catchall].vue
+++ public/src/pages/[...catchall].vue
@@ -1,10 +1,11 @@
+<route>
+meta:
+  title: Not found
+</route>
+
 <template>
-  <div class="flex flex-col justify-center flex-auto">
-    <n-result
-      :status="errorCode"
-      :title="'Error code: ' + errorTitle"
-      :description="props.error ? props.error.toString() : 'You know life is always ridiculous.'"
-    >
+  <div class="p-10">
+    <n-result status="404" title="Page not found" description="You know life is always ridiculous.">
       <template #footer>
         <n-button-group>
           <n-button @click="$router.back()">
@@ -12,50 +13,11 @@
             Go back
           </n-button>
           <n-button @click="$router.push('/')">
-            <n-icon class="mr-1"><i-mdi-home /></n-icon>
-            Home
+            <n-icon class="mr-1"><i-mdi-magnify /></n-icon>
+            Lookup
           </n-button>
         </n-button-group>
       </template>
     </n-result>
   </div>
 </template>
-
-<script setup>
-const props = defineProps({
-  catchall: {
-    type: [String, Array],
-    default: "",
-  },
-  error: {
-    type: Error,
-    default: null,
-  },
-})
-
-const source = computed(() => (typeof props.catchall === "string" ? [props.catchall] : props.catchall))
-const errorCode = ref("0")
-const errorTitle = ref("")
-const supported = ["info", "success", "warning", "error", "404", "403", "500", "418"]
-
-onMounted(() => {
-  for (const item of source.value) {
-    if (supported.includes(item)) {
-      errorCode.value = item
-    } else if (item.match(/^[45][0-9]+$/)) {
-      errorCode.value = "error"
-    } else if (item == "CombinedError") {
-      errorCode.value = "error"
-      errorTitle.value = "query error"
-    } else {
-      errorCode.value = "404"
-      errorTitle.value = "not found"
-      break
-    }
-  }
-
-  if (errorTitle.value == "") {
-    errorTitle.value = errorCode.value
-  }
-})
-</script>
