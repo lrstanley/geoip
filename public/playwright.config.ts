b1f5ff950a2367859d91ed5ca00df4eebe7cb9f9
Liam Stanley <me@liamstanley.io>
2022-12-17 13:35:08 -0500

    minor tweaks to e2e tests
    Signed-off-by: Liam Stanley <me@liamstanley.io>

--- public/playwright.config.ts
+++ public/playwright.config.ts
@@ -7,25 +7,21 @@ const config: PlaywrightTestConfig = {
   webServer: {
     command: "cd .. && make node-preview",
     port: 8081,
-    timeout: 30 * 1000,
     reuseExistingServer: !process.env.CI,
   },
   use: {
-    actionTimeout: 0,
     baseURL: "http://localhost:8081",
     trace: "on-first-retry",
     bypassCSP: true,
     screenshot: "only-on-failure",
   },
-  timeout: 30 * 1000,
   expect: {
     timeout: 7000,
   },
-  fullyParallel: true,
   // repeatEach: 3,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 3 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  // workers: process.env.CI ? 1 : undefined,
   reporter: process.env.CI ? "github" : "list",
   projects: [
     {
@@ -40,25 +36,6 @@ const config: PlaywrightTestConfig = {
         ...devices["Desktop Firefox"],
       },
     },
-    {
-      name: "webkit",
-      use: {
-        ...devices["Desktop Safari"],
-      },
-    },
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: {
-    //     ...devices['Pixel 5'],
-    //   },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: {
-    //     ...devices['iPhone 12'],
-    //   },
-    // },
   ],
 }
 
